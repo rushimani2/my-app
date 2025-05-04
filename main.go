3671449713e9257ff75026150859a10aab9ee25a
Rushimanikanata Lingam <158359799+rushimani2@users.noreply.github.com>
2025-05-05 00:24:56 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,70 +1,72 @@
 package main
- 
+
 import (
     "github.com/aws/constructs-go/constructs/v10"
     "github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2"
     "github.com/cdk8s-team/cdk8s-plus-26-go/cdk8splus26/v2"
+    "github.com/aws/jsii-runtime-go"
 )
+
 type MyChartProps struct {
-	cdk8s.ChartProps
+    cdk8s.ChartProps
 }
 
 func NewMyChart(scope constructs.Construct, id string, props *MyChartProps) cdk8s.Chart {
-	chart := cdk8s.NewChart(scope, &id, &props.ChartProps)
+    chart := cdk8s.NewChart(scope, jsii.String(id), &props.ChartProps)
 
-	appLabels := map[string]*string{
-		"app": cdk8s.String("go-web-app"),
-	}
+    appLabels := map[string]*string{
+        "app": jsii.String("go-web-app"),
+    }
 
-	// Deployment
-	cdk8splus.NewDeployment(chart, cdk8s.String("go-web-app-deployment"), &cdk8splus.DeploymentProps{
-		Metadata: &cdk8s.ApiObjectMetadata{
-			Name:   cdk8s.String("go-web-app"),
-			Labels: &appLabels,
-		},
-		Replicas: cdk8s.Float64(2),
-		Containers: &[]*cdk8splus.ContainerProps{
-			{
-				Image: cdk8s.String("your-dockerhub-username/go-web-app:latest"), // Replace with actual image
-				Port:  cdk8s.Float64(8080),
-			},
-		},
-	})
+    // Deployment
+    deployment := cdk8splus26.NewDeployment(chart, jsii.String("go-web-app-deployment"), &cdk8splus26.DeploymentProps{
+        Metadata: &cdk8s.ApiObjectMetadata{
+            Name:   jsii.String("go-web-app"),
+            Labels: &appLabels,
+        },
+        Replicas: jsii.Number(2),
+        Containers: &[]*cdk8splus26.ContainerProps{
+            {
+                Image: jsii.String("your-dockerhub-username/go-web-app:latest"), // Replace with actual image
+                Port:  jsii.Number(8080),
+            },
+        },
+    })
 
-	// Service
-	cdk8splus.NewService(chart, cdk8s.String("go-web-app-service"), &cdk8splus.ServiceProps{
-		Metadata: &cdk8s.ApiObjectMetadata{
-			Name: cdk8s.String("go-web-app"),
-		},
-		Type: cdk8splus.ServiceType_LOAD_BALANCER,
-		Ports: &[]*cdk8splus.ServicePort{
-			{
-				Port:       cdk8s.Float64(80),
-				TargetPort: cdk8splus.IntOrString_FromNumber(cdk8s.Float64(8080)),
-			},
-		},
-		Selector: &appLabels,
-	})
+    // Service
+    svc := cdk8splus26.NewService(chart, jsii.String("go-web-app-service"), &cdk8splus26.ServiceProps{
+        Metadata: &cdk8s.ApiObjectMetadata{
+            Name: jsii.String("go-web-app"),
+        },
+        Type: cdk8splus26.ServiceType_LOAD_BALANCER,
+        Ports: &[]*cdk8splus26.ServicePort{
+            {
+                Port:       jsii.Number(80),
+                TargetPort: cdk8splus26.IntOrString_FromNumber(jsii.Number(8080)),
+            },
+        },
+        Selector: &appLabels,
+    })
 
-	// Ingress (optional)
-	cdk8splus.NewIngress(chart, cdk8s.String("go-web-app-ingress"), &cdk8splus.IngressProps{
-		Metadata: &cdk8s.ApiObjectMetadata{
-			Name: cdk8s.String("go-web-app"),
-		},
-		Rules: &[]*cdk8splus.IngressRule{
-			{
-				Path:     cdk8s.String("/"),
-				PathType: cdk8splus.HttpIngressPathType_PREFIX,
-				Backend:  cdk8splus.IngressBackend_FromService(cdk8splus.Service_FromServiceName(chart, cdk8s.String("go-web-app-service-ref"), cdk8s.String("go-web-app"), cdk8splus.ServiceType_LOAD_BALANCER)),
-			},
-		},
-	})
+    // Ingress
+    cdk8splus26.NewIngress(chart, jsii.String("go-web-app-ingress"), &cdk8splus26.IngressProps{
+        Metadata: &cdk8s.ApiObjectMetadata{
+            Name: jsii.String("go-web-app"),
+        },
+        Rules: &[]*cdk8splus26.IngressRule{
+            {
+                Path:     jsii.String("/"),
+                PathType: cdk8splus26.HttpIngressPathType_PREFIX,
+                Backend:  cdk8splus26.IngressBackend_FromService(svc),
+            },
+        },
+    })
 
-	return chart
+    return chart
 }
 
 func main() {
-	app := cdk8s.NewApp(nil)
-	NewMyChart(app, "go-web-app", &MyChartProps{})
-	app.Synth()
+    app := cdk8s.NewApp(nil)
+    NewMyChart(app, "go-web-app", &MyChartProps{})
+    app.Synth()
 }
